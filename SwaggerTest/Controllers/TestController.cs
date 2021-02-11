d5f6907485b61a809f7275302816f86349231353
crobibero <cody@robibe.ro>
2021-02-11 14:19:37 -0700

    Initial commit

---
+++ SwaggerTest/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerTest.Controllers
+{
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(
+            [FromQuery, Required] int int2,
+            [FromQuery] string? string1,
+            [FromQuery] string string2,
+            [FromQuery] int? int1 = null)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Index([FromBody] TestDto dto)
+        {
+            return Ok();
+        }
+    }
+
+    public class TestDto
+    {
+        public int? NullableInt { get; set; }
+        public int NonNullableInt { get; set; }
+        public string? NullableString { get; set; }
+        public string NonNullableString { get; set; }
+    }
+}
